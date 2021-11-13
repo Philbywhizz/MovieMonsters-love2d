d53354994daca1c958e26b89a335daf54e4a59a5
Phil Howlett <phil.g.howlett@gmail.com>
2021-11-13 23:55:04 +1000

    Empty project

---
+++ conf.lua
@@ -0,0 +1,11 @@
+---
+--- Configuration file
+---
+
+function love.conf(t)
+    t.version = "11.3"
+
+    t.window.title = "Movie Monsters"
+    t.window.width = 1280
+    t.window.height = 720
+end
---
+++ main.lua
@@ -0,0 +1,14 @@
+---
+--- Movie Monsters
+---
+
+function love.draw()
+    love.graphics.print("Movie Monster", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
+end
+
+function love.keypressed(key)
+    -- Global keys
+    if key == "escape" then
+        love.event.quit()
+    end
+end
