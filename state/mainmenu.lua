f31743aba4dcab99b3da646d66dd5c1032b3af98
Phil Howlett <phil.g.howlett@gmail.com>
2022-01-08 11:02:36 +1000

    Change the main menu layout to be a bit more friendly to the eyes

--- state/mainmenu.lua
+++ state/mainmenu.lua
@@ -1,58 +1,74 @@
 --
 -- a simple mainMenu
 --
-
 local mainMenu = {}
 
 local Slab = require 'lib.slab'
 local movieState = require("state.movie")
 local nyiState = require("state.nyi")
 
-function mainMenu:enter()
-    print("mainMenu:enter()")
+function mainMenu:init()
+    print("mainMenu:init()")
+    love.graphics.setBackgroundColor(0, 0, 0)
+    Slab.SetINIStatePath(nil)
     Slab.Initialize()
+    self.imgMonster = love.graphics.newImage("assets/monster.png")
 end
+
+function mainMenu:enter() print("mainMenu:enter()") end
 
 function mainMenu:update(dt)
     Slab.Update(dt)
 
-    Slab.BeginWindow("mainMenu", {Title="Movie Monsters", AllowMove=false, ShowMinimize=false})
+    Slab.BeginWindow("mainMenu", {
+        Title = "Movie Monsters",
+        X = love.graphics.getWidth() / 2 - 275,
+        Y = love.graphics.getHeight() / 2 - 150,
+        W = 550,
+        H = 300,
+        AllowMove = false,
+        AutoSizeWindow = false,
+        ShowMinimize = false
+    })
 
-    if Slab.Button("Enter Movie") then
-        GameState.push(movieState)
-    end
+    Slab.BeginLayout("mainMenu-layout", {
+        AlignX = "center",
+        AlignY = "center",
+        AlignRowY = "center",
+        ExpandW = false,
+        Columns = 2
+    })
+    do
+        Slab.SetLayoutColumn(1)
+        Slab.Image("monster", {Image = self.imgMonster})
 
-    if Slab.Button("Ticket Booth") then
-        GameState.push(nyiState)
+        Slab.SetLayoutColumn(2)
+        do
+            local params = {W = 150, H = 50}
+            if Slab.Button("Enter Movie", params) then
+                GameState.push(movieState)
+            end
+            if Slab.Button("Ticket Booth", params) then
+                GameState.push(nyiState)
+            end
+            if Slab.Button("Box Office", params) then
+                GameState.push(nyiState)
+            end
+            if Slab.Button("Manager's Office", params) then
+                GameState.push(nyiState)
+            end
+            if Slab.Button("Quit", params) then love.event.quit() end
+        end
     end
-
-    if Slab.Button("Box Office") then
-        GameState.push(nyiState)
-    end
-
-    if Slab.Button("Manager's Office") then
-        GameState.push(nyiState)
-    end
-
-    if Slab.Button("Quit") then
-        love.event.quit()
-    end
+    Slab.EndLayout()
 
     Slab.EndWindow()
 end
 
-function mainMenu:draw()
-    Slab.Draw()
-end
+function mainMenu:draw() Slab.Draw() end
 
-function mainMenu:keypressed(key)
-    if key == "escape" then
-        love.event.quit()
-    end
-end
+function mainMenu:keypressed(key) if key == "escape" then love.event.quit() end end
 
-function mainMenu:leave()
-    print("mainMenu:leave()")
-end
+function mainMenu:quit() print("mainMenu:quit()") end
 
 return mainMenu
--- state/nyi.lua
+++ state/nyi.lua
@@ -19,7 +19,7 @@ function nyi:draw()
     -- draw previous state screen
     self.from:draw()
     -- now draw nyi message
-    love.graphics.setColor(0 ,0 ,0 , 0.25)
+    love.graphics.setColor(0 ,0 ,0 , 0.75)
     love.graphics.rectangle('fill', 0, 0, W, H)
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.printf('Not Yet Implemented - Press <ESC> to return', 0, H/2, W, 'center')
